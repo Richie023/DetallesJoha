121e8646a3e580d9b850f1094412a0af3e41f470
leonel <lazofeifa30030@ufide.ac.cr>
2024-11-04 22:57:03 -0600

    Squashed commit of the following:
    commit 1943df28aefc4f73d689322faab6579f22dc6709
    Author: Richie023 <85466516+Richie023@users.noreply.github.com>
    Date:   Mon Nov 4 20:33:15 2024 -0600
    
        Cambio controller Empresa
    
    commit 7caffdb0cc11c4fcb963fcbe99b5488a727e0161
    Author: Richie023 <85466516+Richie023@users.noreply.github.com>
    Date:   Mon Nov 4 17:38:05 2024 -0600
    
        Informacion Empresa

---
+++ Proyecto/Api/Api/Controllers/InfoEController.cs
@@ -0,0 +1,75 @@
+﻿using Api.Entidades;
+using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class InfoEController : ApiController
+    {
+
+        [HttpPost]
+
+        [Route("InfoE/AgregarInfoE")]
+
+        public Confirmacion AgregarInfoE(InfoE entidad)
+
+        {
+
+            var respuesta = new Confirmacion();
+
+            try
+
+            {
+
+                using (var db = new DetallesJohaEntities())
+
+                {
+
+                    var resp = db.RegistrarInformacionEmpresa(entidad.Telefono, entidad.CorreoElectronico, entidad.Direccion, entidad.AcercaDeNosotros, entidad.Politicas, entidad.Contactanos, entidad.NombreEmpresa, entidad.TerminosCondiciones, entidad.OrdenesDevoluciones);
+
+                    if (resp > 0)
+
+                    {
+
+                        respuesta.Codigo = 0;
+
+                        respuesta.Detalle = string.Empty;
+
+                    }
+
+                    else
+
+                    {
+
+                        respuesta.Codigo = -1;
+
+                        respuesta.Detalle = "La informacion no se pudo agregar";
+
+                    }
+
+                }
+
+            }
+
+            catch (Exception)
+
+            {
+
+                respuesta.Codigo = -1;
+
+                respuesta.Detalle = "Se presentó un error en el sistema";
+
+            }
+
+            return respuesta;
+
+        }
+
+    }
+
+}
