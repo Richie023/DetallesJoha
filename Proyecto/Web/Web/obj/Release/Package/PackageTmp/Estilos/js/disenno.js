5e871afbd304f33c366267e1ca096fa043b73887
leonel <lazofeifa30030@ufide.ac.cr>
2025-02-14 19:05:56 -0600

    publicacion pidio muchos archivos perdidos en las posibles versiones

---
+++ Proyecto/Web/Web/obj/Release/Package/PackageTmp/Estilos/js/disenno.js
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+const initCanvas = (id) => {
+    return new fabric.Canvas(id, {
+        width:450,
+        height:450,
+       
+       
+    });
+}
+
+
+const canvas = initCanvas('canvas');
+
+
+
+    
