59e9f95a8c6dd106bfcd56e356ee7a2f65123b01
Richie023 <85466516+Richie023@users.noreply.github.com>
2025-03-19 20:01:07 -0600

    Pagina acerca de nosotros
    detalles en layout y footer, creacion de la pagina de acerca de nostros

---
+++ Proyecto/Web/Web/Controllers/AcercaDController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class AcercaDController : Controller
+    {
+        // GET: AcercaD
+        public ActionResult Acerca()
+        {
+            return View();
+        }
+
+    }
+}
